86b062a76b947b13d0a0f6b61bdbb7a3d4d22622
jeffrey ram pineda <jeffreyrampineda@gmail.com>
2020-08-12 21:40:22 -0400

    added: python skill

--- src/components/SkillBars.js
+++ src/components/SkillBars.js
@@ -4,22 +4,28 @@ const SkillBars = _ => {
   return (
     <div className="content" style={{ float: "right" }}>
       <ul>
-        <li>
+        <li class="skill-item">
           <p class="skill-label">HTML</p>
           <div class="progress">
             <div class="progress-value" style={{ width: "80%" }}></div>
           </div>
         </li>
-        <li>
+        <li class="skill-item">
           <p class="skill-label">CSS</p>
           <div class="progress">
             <div class="progress-value" style={{ width: "75%" }}></div>
           </div>
         </li>
-        <li>
+        <li class="skill-item">
           <p class="skill-label">JS</p>
           <div class="progress">
             <div class="progress-value" style={{ width: "95%" }}></div>
+          </div>
+        </li>
+        <li class="skill-item">
+          <p class="skill-label">Python</p>
+          <div class="progress">
+            <div class="progress-value" style={{ width: "80%" }}></div>
           </div>
         </li>
       </ul>
