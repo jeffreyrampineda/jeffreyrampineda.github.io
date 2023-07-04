132f01eac36b5842e98417be43bb4710f699f91d
jeffrey ram pineda <jeffreyrampineda@gmail.com>
2023-07-04 11:48:21 -0400

    set up for gh pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// @ts-check
 
-module.exports = nextConfig
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+  output: "export",
+  experimental: {
+    appDir: true,
+  },
+};
+
+module.exports = nextConfig;
